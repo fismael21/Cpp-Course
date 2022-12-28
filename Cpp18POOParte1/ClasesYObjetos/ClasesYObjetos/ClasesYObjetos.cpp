4b0322d6fe70eeb4708dd90e5efed0858f27849f
Fernando Canul Caballero <fernandoismaelvirgo@gmail.com>
2022-12-28 16:29:52 -0600

    Update

--- Cpp18POOParte1/ClasesYObjetos/ClasesYObjetos/ClasesYObjetos.cpp
+++ Cpp18POOParte1/ClasesYObjetos/ClasesYObjetos/ClasesYObjetos.cpp
@@ -9,10 +9,10 @@ int main(int argcs, char** argv)
 {
     cout << "Hello World!\n";
 
-    Punto punto(2,3); //Creación de un objeto estatico
+    Punto punto(2,3);
     cout << "El valor de X es: " << punto.getX() << " y el valor de Y es: " << punto.getY() << endl;
 
-    Punto* punto2 = new Punto(); //Creación de objeto dinamico
+    Punto* punto2 = new Punto();
     punto2->setX(4);
     punto2->setY(7);
 
